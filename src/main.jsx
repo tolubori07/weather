32d63892d9d14fe60bf65d0aaa124a5e19d6d7a1
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-26 01:18:54 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode className="my-auto w-[98%]">
     <App />
   </React.StrictMode>,
 )
